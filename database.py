15c6190282c1e0fcad6af3292e2c9125f117e7ad
Tee <tejas.tee@gmail.com>
2020-10-03 16:51:32 +0530

    Added checks for 300s Time Limit Rule in PAW

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import mysql.connector
 import logging
 import constants
 from MusicObj import MusicFile
+
 '''
  CREATE USER 'root' IDENTIFIED BY 'password';
 
@@ -17,16 +18,16 @@ CREATE TABLE `tejastee4$music_service`.`music_data` (
 
 '''
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:  %(message)s', handlers=[logging.StreamHandler()])
 
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s:  %(message)s',handlers=[logging.StreamHandler()])
 
 def sample():
     logging.debug("Connecting . . ")
     mydb = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='wqsa@12',
-            database='testdb'
+        host='localhost',
+        user='root',
+        password='wqsa@12',
+        database='testdb'
     )
     logging.debug("Connected!")
 
@@ -45,6 +46,11 @@ def sample():
 
 class MySqlDatabase:
     def __init__(self):
+        self.mydb = None
+        self.cursor = None
+        self.connect()
+
+    def connect(self):
         logging.debug("Connecting......")
         self.mydb = mysql.connector.connect(
             **constants.DB_CRED
@@ -56,24 +62,39 @@ class MySqlDatabase:
         logging.debug("Commit DB!")
         self.mydb.commit()
 
+    def check_conn(self):
+        if self.mydb and self.mydb.is_connected():
+            return True
+        else:
+            self.connect()
+            return True
+
 
 class MusicData(MySqlDatabase):
     def __init__(self):
         MySqlDatabase.__init__(self)
         logging.debug("Music Data!")
 
+    def _check_connection(func):
+        def _wrapper(self, *args, **kwargs):
+            if self.check_conn():
+                return func(self, *args, **kwargs)
+        return _wrapper
+
+    @_check_connection
     def get_all(self):
-        self.cursor.execute("select title, artist_name, album, genre, CAST(length AS CHAR) length, path from music_data")
+        self.cursor.execute(
+            "select title, artist_name, album, genre, CAST(length AS CHAR) length, path from music_data")
         myresult = self.cursor.fetchall()
         logging.debug(myresult)
         return myresult
 
+    @_check_connection
     def insert_to_music_data(self, music_object):
         logging.debug("Inserting - {}".format(music_object))
-        music_object: MusicFile
-
         sql = "INSERT IGNORE INTO music_data (title, artist_name, album, genre, length, path) VALUES (%s, %s, %s, %s, %s, %s)"
-        values = [music_object.title, music_object.artist_name, music_object.album, music_object.genre, music_object.length, music_object.path]
+        values = [music_object.title, music_object.artist_name, music_object.album, music_object.genre,
+                  music_object.length, music_object.path]
         self.cursor.execute(sql, values)
         self.commit()
         print(self.cursor.rowcount, "record inserted.")
@@ -81,4 +102,5 @@ class MusicData(MySqlDatabase):
 
 if __name__ == '__main__':
     db = MusicData()
+    print(db)
     db.get_all()
